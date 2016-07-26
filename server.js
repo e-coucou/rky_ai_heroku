6ea67423705686ca294255d2297116568a955462
e-coucou <eric@ecoucou.com>
2016-07-26 22:34:54 +0200

    map update

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.post("/api/v1/map", function(req, res) {
     newUser.date = d.toUTCString();
     newUser.UTC = d.getTime();
     newUser.json = d.toJSON();
-  
+
 
   if (!(req.body.source || req.body.latitude)) {
     handleError(res, "Donnees Invalides", "Doit comporter la source et/ou localisation.", 400);
@@ -105,7 +105,11 @@ app.get("/api/v1/user/:id", function(req, res) {
 app.put("/api/v1/map/:id", function(req, res) {
   var updateDoc = req.body;
   delete updateDoc._id;
-
+  var d = new Date();
+    updateDoc.date = d.toUTCString();
+    updateDoc.UTC = d.getTime();
+    updateDoc.json = d.toJSON();
+  console(updateDoc);
   db.collection(RKYAI_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
     if (err) {
       handleError(res, err.message, "Failed: impossible de mettre à jour le user");
